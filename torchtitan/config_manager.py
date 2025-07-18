e24518ca36d33afa99f1d48fd22caa96ba72bfc1
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-07-17 17:12:24 -0700

    allow specifying ft pg
    Summary:
    - allow using gloo process group
    - add a parameter to the ft config
    - only nccl and gloo will be supported for now

--- torchtitan/config_manager.py
+++ torchtitan/config_manager.py
@@ -606,6 +606,11 @@ class FaultTolerance:
     Note that this is still an experimental feature.
     """
 
+    process_group: str = "gloo"
+    """
+    The process group to use for fault tolerance. Currently, only "gloo" and "nccl" are supported
+    """
+
     replica_id: int = 0
     """The TorchFT replica ID of this run."""
 
