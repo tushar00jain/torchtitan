870f0ba3e134fa5f2ba54314fb8d695c08316aa6
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-07-30 20:59:27 -0700

    model fragments for diloco
    Summary:
    - add a configuration option for users to provide how they want to partition the model
    - if this is provided, the model needs to implement `FaultTolerantTrainingSpec` that defines the framentation function to split the model based on the configuration
    - determine the model fragments in training script to pass to ft manager
    
    Test Plan:
    Running llama3 8b parameters with 2 fragments, 1 step delay, each fragment gets synced every 20 steps
    
    <img width="944" height="545" alt="image" src="https://github.com/user-attachments/assets/6d16f486-7260-49d6-8ba3-3e98cd331e58" />

--- torchtitan/models/llama3/__init__.py
+++ torchtitan/models/llama3/__init__.py
@@ -4,13 +4,15 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from torchtitan.components.ft import FaultTolerantTrainSpec
 from torchtitan.components.loss import build_cross_entropy_loss
 from torchtitan.components.lr_scheduler import build_lr_schedulers
 from torchtitan.components.optimizer import build_optimizers
 from torchtitan.components.tokenizer import build_hf_tokenizer
 from torchtitan.components.validate import build_validator
 from torchtitan.datasets.hf_datasets import build_hf_dataloader
-from torchtitan.protocols.train_spec import register_train_spec, TrainSpec
+from torchtitan.distributed.fault_tolerance import fragment_llm
+from torchtitan.protocols.train_spec import register_train_spec
 
 from .infra.parallelize import parallelize_llama
 from .infra.pipeline import pipeline_llama
@@ -71,12 +73,13 @@ llama3_configs = {
 
 
 register_train_spec(
-    TrainSpec(
+    FaultTolerantTrainSpec(
         name="llama3",
         model_cls=Transformer,
         model_args=llama3_configs,
         parallelize_fn=parallelize_llama,
         pipelining_fn=pipeline_llama,
+        fragment_fn=fragment_llm,
         build_optimizers_fn=build_optimizers,
         build_lr_schedulers_fn=build_lr_schedulers,
         build_dataloader_fn=build_hf_dataloader,
