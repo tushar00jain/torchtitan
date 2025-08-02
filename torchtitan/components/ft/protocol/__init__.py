2f08f75354e1ef5d5c93ddcbc1dcf5f07da3de45
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-08-02 15:13:42 -0700

    model fragments for diloco
    Summary:
    - add a configuration option for users to provide how they want to partition the model
    - if this is provided, the model needs to implement `FaultTolerantTrainingSpec` that defines the framentation function to split the model based on the configuration
    - determine the model fragments in training script to pass to ft manager
    
    Test Plan:
    Running llama3 8b parameters with 2 fragments, 1 step delay, each fragment gets synced every 20 steps
    
    <img width="944" height="545" alt="image" src="https://github.com/user-attachments/assets/6d16f486-7260-49d6-8ba3-3e98cd331e58" />

---
+++ torchtitan/components/ft/protocol/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Import to register quantization modules.
+from torchtitan.components.ft.protocol.model import (
+    FaultTolerantTrainSpec,
+)
+
+
+__all__ = [
+    "FaultTolerantTrainSpec",
+]
