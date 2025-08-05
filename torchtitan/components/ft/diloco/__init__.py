e4f0f8d96b40cf939ef3dbdd4b6193b91fe35f65
Tushar Jain <tushar00jain@users.noreply.github.com>
2025-08-05 10:56:56 -0700

    separate out diloco configs

--- torchtitan/components/ft/diloco/__init__.py
+++ torchtitan/components/ft/diloco/__init__.py
@@ -4,10 +4,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from torchtitan.components.ft.diloco.job_config import FaultTolerance, JobConfig
 from torchtitan.components.ft.diloco.protocol import FaultTolerantTrainSpec
 from torchtitan.components.ft.diloco.utils import fragment_llm
 
+
 __all__ = [
+    "FaultTolerance",
+    "JobConfig",
     "FaultTolerantTrainSpec",
     "fragment_llm",
 ]
